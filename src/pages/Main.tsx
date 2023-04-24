50a224ad2675a9586f64e8a82b257a41c9c04a17
Arthur Levy <arthurlbvieiraa@gmail.com>
2023-04-23 21:28:02 -0300

    CORREÇÃO DE ERROS

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -11,15 +11,22 @@ export function Main(){
 	let [finalMontant, setFinalMontant] = useState<number>(0);	
 	let [result, setResult] = useState<number>(0);
 	let [visible, setVisible] = useState<boolean>(false);
-	let container = useRef<HTMLDivElement>(null)
-	let [anos, setAnos] = useState<number>(0)
-	let [meses, setmeses] = useState<number>(0)
-	let [dias, setdias] = useState<number>(0)
+	let container = useRef<HTMLDivElement>(null);
+	let [anos, setAnos] = useState<number>(0);
+	let [meses, setmeses] = useState<number>(0);
+	let [dias, setdias] = useState<number>(0);
+
+
 	
-	function calculate(): void {	
-		let firstLog: number = Number((Math.log10(finalMontant / initialMontant)).toFixed(2));
-		let secondLog: number = Number((Math.log10(index + 1.0)).toFixed(2))		
-		setResult(Number((firstLog / secondLog).toFixed(2)));	
+	function calculate(): void {
+	  let firstLog: number = Number((Math.log10(finalMontant / initialMontant)));
+	  let secondLog: number = Number((Math.log10(index + 1.0)));
+	  let resultado: number = (firstLog / secondLog);
+	  setResult(resultado)
+	  setAnos(Math.floor(resultado));
+	  setmeses(Math.floor((resultado - Math.floor(resultado)) * 12));	  
+	  console.log()
+	  setdias(Math.floor((meses - Math.floor(meses)) * 30));
 	};
 
 	function showAnwser(): void{		
@@ -27,7 +34,6 @@ export function Main(){
 			container.current.style.opacity = '0';
 			container.current.style.visibility = 'hidden';
 			setVisible(false);
-			setResult(0);
 		}else if(container.current && !visible){
 			container.current.style.opacity = '1';
 			container.current.style.visibility = 'initial';
@@ -52,10 +58,8 @@ export function Main(){
 			<Container ref={container}>
 				<button onClick={() => showAnwser()}>Voltar</button>
 				<p>						
-				  O tempo necessário para o montante final ser de {finalMontant} a uma taxa de {`${index * 100}%`} é de {result.toFixed(0)} anos  { ((result - (parseInt(String(result)))) * 12).toFixed(0)} meses {((result - (parseInt(String(result)))) * 12 * 30).toFixed(0)} dias.
+				  O tempo necessário para o montante final ser de {finalMontant} a uma taxa de {`${index * 100}%`} é de {anos} anos  {meses} meses {dias} dias.
 				</p>
-
-
 			</Container>		
 			
 		</>
